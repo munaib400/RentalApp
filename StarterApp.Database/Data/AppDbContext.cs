a42d3ba35a5a74efc10f4a2cded24deaecd84aec
munaib400 <munaib400@gmail.com>
2026-04-30 18:52:17 +0100

    Add unit tests - all 6 passing

--- StarterApp.Database/Data/AppDbContext.cs
+++ StarterApp.Database/Data/AppDbContext.cs
@@ -37,6 +37,8 @@ public class AppDbContext : DbContext
     public DbSet<Role> Roles { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
