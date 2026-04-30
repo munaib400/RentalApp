140c1fa87333830b8fe271ce24725e32b83426e7
munaib400 <munaib400@gmail.com>
2026-04-30 19:24:14 +0000

    Add generic IRepository<T> interface

---
+++ StarterApp/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+namespace StarterApp.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
